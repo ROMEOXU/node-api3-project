89083aab89098b9c81db28450dc7634615a416f9
Mac <xuromeo@gmail.com>
2020-07-27 21:46:22 -0700

    up

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+
+const server = require('./server.js');
+server.listen(4000,()=>{
+    console.log('romeo is listening on 4000')
+});
+
+// server.use(function(req,res,next){
+//     console.log('making own middleware now ');
+//     next();
+//   });
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter');//route step
 const server = express();
-
+server.use(express.json());
+//need put ahead ,otherwise will not show
+server.use(logger);
+server.use('/api/users',userRouter);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log('logged!',req.method);
+  next();
+}
+
 
 module.exports = server;
