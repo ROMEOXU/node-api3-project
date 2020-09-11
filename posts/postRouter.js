96f838769d05b6ec8c0d580591d6142af274dcb8
Mac <xuromeo@gmail.com>
2020-09-10 19:47:16 -0700

    post up

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,56 @@
 const express = require('express');
-
+const Posts = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  Posts.get()
+  .then(posts=>res.status(200).json(posts))
+  .catch(err=>res.status(500).json(err))
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId,(req, res) => {
   // do your magic!
+  Posts.getById(req.params.id)
+  .then(post=>res.status(200).json(post))
+  .catch(err=>res.status(500).json(err))
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId,(req, res) => {
   // do your magic!
+  Posts.remove(req.params.id)
+  .then(removePost=>res.status(200).json(removePost))
+  .catch(err=>res.status(500).json(err))
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId,(req, res) => {
   // do your magic!
+  Posts.update(req.post.id,req.body)
+  .then(count=>{
+    if (count = 1){
+      res.status(201).json({message:'post was successfully updated'})
+    }else{res.status(500).json({error:"there is error updating"})}
+  })
+  .catch(err=>res.status(500).json({err:err.message}))
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  Posts.getById(req.params.id)
+  .then(post => {
+    if(post && post.id) {
+      req.post = post;
+      
+      next();
+    } else {
+      res.status(400).json({  message: "Invalid post id." });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({error: err.message})
+  })
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const userRouter = require('./users/userRouter');//route step
+const userRouter = require('./users/userRouter');//import user router
+const postRouter = require('./posts/postRouter');//import post router
 const server = express();
 server.use(express.json());
 //need put ahead ,otherwise will not show
 server.use(logger);
 server.use('/api/users',userRouter);
+server.use('/api/post', postRouter);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
